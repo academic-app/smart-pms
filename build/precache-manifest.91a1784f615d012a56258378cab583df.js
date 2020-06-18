eb58cbc66ee6672df670dc1dd2445c4a856cb87c
Sijan Maharjan <path.sijan@gmail.com>
2020-06-18 11:54:06 +0545

    update

---
+++ build/precache-manifest.91a1784f615d012a56258378cab583df.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b172754d01979a394fa1392d89e5afff",
+    "url": "/smart-task-management-system/index.html"
+  },
+  {
+    "revision": "6b519f1a30820d68700f",
+    "url": "/smart-task-management-system/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "a6628464a18957bbb0b5",
+    "url": "/smart-task-management-system/static/js/2.7503df23.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/smart-task-management-system/static/js/2.7503df23.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6b519f1a30820d68700f",
+    "url": "/smart-task-management-system/static/js/main.44458dd8.chunk.js"
+  },
+  {
+    "revision": "229dd4acbe36cfa586d0",
+    "url": "/smart-task-management-system/static/js/runtime-main.214541b9.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/smart-task-management-system/static/media/logo.5d5d9eef.svg"
+  }
+]);
