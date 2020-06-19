81f81c349bd918e94a88ad84757989800beedbca
Sijan Maharjan <path.sijan@gmail.com>
2020-06-19 20:42:07 +0545

    update

---
+++ build/precache-manifest.5d05d0dbaa50332fca88619e56e2b480.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dc9d5d751d542b5969a1f1369e386ad1",
+    "url": "/smart-task-management-system/index.html"
+  },
+  {
+    "revision": "a76f3d40114ffaaf2f63",
+    "url": "/smart-task-management-system/static/css/2.5f6aaac1.chunk.css"
+  },
+  {
+    "revision": "7c865497fa3f8829affe",
+    "url": "/smart-task-management-system/static/css/main.fdc0fa7f.chunk.css"
+  },
+  {
+    "revision": "a76f3d40114ffaaf2f63",
+    "url": "/smart-task-management-system/static/js/2.49f84451.chunk.js"
+  },
+  {
+    "revision": "f4aac9c965aa45c554c7c1d1e3f69ce3",
+    "url": "/smart-task-management-system/static/js/2.49f84451.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7c865497fa3f8829affe",
+    "url": "/smart-task-management-system/static/js/main.dbae1aec.chunk.js"
+  },
+  {
+    "revision": "229dd4acbe36cfa586d0",
+    "url": "/smart-task-management-system/static/js/runtime-main.214541b9.js"
+  },
+  {
+    "revision": "e1a1bef5fb30519caf3b020006e0873b",
+    "url": "/smart-task-management-system/static/media/boards.e1a1bef5.svg"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/smart-task-management-system/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/smart-task-management-system/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/smart-task-management-system/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/smart-task-management-system/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/smart-task-management-system/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
