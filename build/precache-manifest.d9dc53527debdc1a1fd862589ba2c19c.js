c7eb209cfae518963a326ecd563dcd85cae7d9dd
Sijan Maharjan <path.sijan@gmail.com>
2020-06-23 03:27:14 +0545

    update deploy

---
+++ build/precache-manifest.d9dc53527debdc1a1fd862589ba2c19c.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e068601efe75096601bcf01eacc3e65a",
+    "url": "/smart-task-management-system/index.html"
+  },
+  {
+    "revision": "d63691574ba8770f1bf3",
+    "url": "/smart-task-management-system/static/css/2.0040f42c.chunk.css"
+  },
+  {
+    "revision": "7dcfd32f3283ed536c92",
+    "url": "/smart-task-management-system/static/css/main.99d0a2ca.chunk.css"
+  },
+  {
+    "revision": "d63691574ba8770f1bf3",
+    "url": "/smart-task-management-system/static/js/2.236b18dc.chunk.js"
+  },
+  {
+    "revision": "fcca2cdcc5e6967026a1ba00172f2d47",
+    "url": "/smart-task-management-system/static/js/2.236b18dc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7dcfd32f3283ed536c92",
+    "url": "/smart-task-management-system/static/js/main.9974afa8.chunk.js"
+  },
+  {
+    "revision": "229dd4acbe36cfa586d0",
+    "url": "/smart-task-management-system/static/js/runtime-main.214541b9.js"
+  },
+  {
+    "revision": "e1a1bef5fb30519caf3b020006e0873b",
+    "url": "/smart-task-management-system/static/media/boards.e1a1bef5.svg"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/smart-task-management-system/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/smart-task-management-system/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/smart-task-management-system/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/smart-task-management-system/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/smart-task-management-system/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "7b97f5e3e2328ef272d8e59534bf00f8",
+    "url": "/smart-task-management-system/static/media/form-layout.7b97f5e3.svg"
+  },
+  {
+    "revision": "9304ee216563f9d3270ce52a1f1ce867",
+    "url": "/smart-task-management-system/static/media/form-layout.9304ee21.svg"
+  }
+]);
